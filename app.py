f3ead7262c3ab124f5db7d73c20b67d73c4e313b
Parth <parthjoshi0990@gmail.com>
2024-07-24 12:17:40 +0530

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify
 from dotenv import load_dotenv
-from dbconfig import connect
+from dbconfig import connect, connectDynamicDB
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,15 +9,25 @@ load_dotenv()
 app = Flask(__name__)
 
 # Example route to demonstrate accessing environment variables
+# @app.route('/')
+# def index(): # Default value can be specified
+#     conn = connect()
+#     Qry = "select * from user; select * from user;";
+#     cursor = conn.cursor(dictionary=True)
+#     cursor.execute(Qry)
+#     users = cursor.fetchall()
+
+#     return jsonify({'users': users})
+
 @app.route('/')
 def index(): # Default value can be specified
-    conn = connect()
+    conn = connectDynamicDB("root",'a1', 'localhost', 'test')
     Qry = "select * from user; select * from user;";
     cursor = conn.cursor(dictionary=True)
     cursor.execute(Qry)
     users = cursor.fetchall()
 
-    return jsonify({'users': users})
+    return jsonify({'users': users}, {'success': True})
 
 if __name__ == '__main__':
     app.run()
--- dbconfig.py
+++ dbconfig.py
@@ -11,8 +11,20 @@ def connect():
         if conn.is_connected():
             return conn
         else:
-            return "Verify Db Config"
+            print("Verify Config Details")
     except Exception as e:
         print(e)
     finally:
         print("end")
+
+def connectDynamicDB(_username: str, _passowrd: str, _host: str, _dbname: str):
+    try:
+        conn = mysql.connector.connect(host=_host, user=_username, password=_passowrd, database=_dbname)
+        if conn.is_connected():
+            return conn
+        else:
+            print("Verify Config Details")
+    except Exception as e:
+        print(e)
+    finally:
+        print("done")
