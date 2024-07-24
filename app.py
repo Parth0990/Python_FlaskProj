7f975d53d35aef4001276aa6b49169c49277721a
Parth <parthjoshi0990@gmail.com>
2024-07-24 13:10:44 +0530

    third commit

--- app.py
+++ app.py
@@ -9,25 +9,25 @@ load_dotenv()
 app = Flask(__name__)
 
 # Example route to demonstrate accessing environment variables
-# @app.route('/')
-# def index(): # Default value can be specified
-#     conn = connect()
-#     Qry = "select * from user; select * from user;";
-#     cursor = conn.cursor(dictionary=True)
-#     cursor.execute(Qry)
-#     users = cursor.fetchall()
-
-#     return jsonify({'users': users})
-
 @app.route('/')
 def index(): # Default value can be specified
-    conn = connectDynamicDB("root",'a1', 'localhost', 'test')
+    conn = connect()
     Qry = "select * from user; select * from user;";
     cursor = conn.cursor(dictionary=True)
     cursor.execute(Qry)
     users = cursor.fetchall()
 
-    return jsonify({'users': users}, {'success': True})
+    return jsonify({'users': users})
+
+@app.route('/home')
+def home(): # Default value can be specified
+    conn1 = connectDynamicDB()
+    Qry = "select * from user; select * from user;";
+    cursor1 = conn1.cursor(dictionary=True)
+    cursor1.execute(Qry)
+    users = cursor1.fetchall()
+
+    return jsonify({"users": users})
 
 if __name__ == '__main__':
     app.run()
--- dbconfig.py
+++ dbconfig.py
@@ -17,8 +17,14 @@ def connect():
     finally:
         print("end")
 
-def connectDynamicDB(_username: str, _passowrd: str, _host: str, _dbname: str):
+def connectDynamicDB():
     try:
+        SetDynamicDBConfig()
+        _username = os.getenv("Dynamic_Username");
+        _passowrd = os.getenv("Dynamic_Password");
+        _host = os.getenv("Dynamic_Host");
+        _dbname = os.getenv("Dynamic_DBName");
+        print("username:" + _username + ", Password: " + _passowrd + ", host: " + _host + ", dbname: " + _dbname)
         conn = mysql.connector.connect(host=_host, user=_username, password=_passowrd, database=_dbname)
         if conn.is_connected():
             return conn
@@ -28,3 +34,19 @@ def connectDynamicDB(_username: str, _passowrd: str, _host: str, _dbname: str):
         print(e)
     finally:
         print("done")
+
+def SetDynamicDBConfig():
+    try:
+        conn = connect()
+        Qry = "Select * from DBSetting;";
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute(Qry)
+        dbconfig = cursor.fetchall()
+        os.environ["Dynamic_Username"]= dbconfig[0]["username"]
+        os.environ["Dynamic_Password"]= dbconfig[0]["password"]
+        os.environ["Dynamic_Host"]= dbconfig[0]["host"]
+        os.environ["Dynamic_DBName"]= dbconfig[0]["dbname"]
+    except Exception as e:
+        print(e)
+    finally:
+        print("closed")
