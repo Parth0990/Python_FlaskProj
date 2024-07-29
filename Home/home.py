86cf32ac7f20b718d48e2180d06012d95cf0edf1
Parth <parthjoshi0990@gmail.com>
2024-07-29 18:29:15 +0530

    14th Commit

--- Home/home.py
+++ Home/home.py
@@ -8,25 +8,6 @@ home_blueprint = Blueprint('admin', __name__)
 def admin_index():
     return jsonify({"message": "Welcome to the admin dashboard"})
 
-@home_blueprint.route('/user', methods=["GET"])
-def index(): # Default value can be specified
-    try:
-        conn = connect()
-        if conn is not None:
-            Qry = "select * from UserData;";
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute(Qry)
-            users = cursor.fetchall()
-            return jsonify({"users": users}, {"Success": True})
-        else:
-            return jsonify({"Error": {"Message": "Please Verify DB Settings"}}, {"Success": False})
-    except Exception as e:
-        return jsonify({"Error": {"Message": str(e)}}, {"Success": False})
-    finally:
-        if conn is not None:
-            cursor.close()
-            conn.close()
-
 @home_blueprint.route('/home', methods=["GET"])
 def test1(): # Default value can be specified
     try:
